1db008980ad21773c29cf2d06554794738fbac94
realdaveeeed <fabricziusdavid@gmail.com>
2025-11-27 22:57:29 +0100

    Updated UserView header

--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -5,9 +5,10 @@ namespace ViewModel;
 public partial class UserViewModel : BaseViewModel
 {
     [ObservableProperty] private string _userEmail;
-
+    [ObservableProperty] private string _headerMainText;
     public UserViewModel(string email)
     {
         UserEmail = email;
+        HeaderMainText = "Welcome " + _userEmail;
     }
 }
