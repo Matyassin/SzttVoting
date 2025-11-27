8ddc634f66321ad50fc4263e81300bc64c553128
Mátyás Végh <veghmatya@gmail.com>
2025-11-27 23:39:03 +0100

    Changed welcome message in the User View to the string before their emails to reflect their quote "username" :)

--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -6,9 +6,14 @@ public partial class UserViewModel : BaseViewModel
 {
     [ObservableProperty] private string _userEmail;
     [ObservableProperty] private string _headerMainText;
+
     public UserViewModel(string email)
     {
         UserEmail = email;
-        HeaderMainText = "Welcome " + _userEmail;
+
+        string emailFirstPart = UserEmail.Split('@').First();
+        string userName = char.ToUpper(emailFirstPart[0]) + emailFirstPart.Substring(1);
+
+        HeaderMainText = $"Welcome, {userName}!";
     }
 }
