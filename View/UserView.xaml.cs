dfe297494f5c0f18a3272d8d6aa7fa51fb7427e3
realdaveeeed <fabricziusdavid@gmail.com>
2025-11-28 14:47:02 +0100

    Basic user UI for testing the nested views

--- View/UserView.xaml.cs
+++ View/UserView.xaml.cs
@@ -5,18 +5,40 @@ namespace View;
 public partial class UserView : ContentPage
 {
     private readonly UserViewModel _vm;
-    
+    private List<ContentView> _nestedUserViews = new List<ContentView>();
+    private int _currentNestedViewID;
+
     public UserView(string email)
     {
         InitializeComponent();
         _vm = new UserViewModel(email);
         BindingContext = _vm;
+        
+        _nestedUserViews.Add(new HomepageView());
+        _nestedUserViews.Add(new OngoingVotesView());
+        _currentNestedViewID = 0;
+        
+        DynamicsArea.Content = _nestedUserViews.ElementAt(_currentNestedViewID);
     }
 
     private void Signout_OnClicked(object sender, EventArgs e)
     {
         Application.Current.MainPage = new LoginView();
     }
+
+    private void HomepageHeader_Clicked(object sender, EventArgs e)
+    {
+        _currentNestedViewID = 0;
+        DynamicsArea.Content = _nestedUserViews.ElementAt(_currentNestedViewID);
+    }
+
+    private void OngoingVotesHeader_Clicked(object sender, EventArgs e)
+    {
+        _currentNestedViewID = 1;
+        DynamicsArea.Content = _nestedUserViews.ElementAt(_currentNestedViewID);
+    }
+    
+    
 
     private void LoadInitialContent()
     {
