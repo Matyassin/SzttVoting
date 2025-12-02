88f3107e8b7abb163ace89f60a9f5c6606e32aef
Mátyás Végh <veghmatya@gmail.com>
2025-12-02 17:13:36 +0100

    Refactoring code :)

--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -1,18 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Services;
+﻿using Services;
 
 namespace ViewModel;
 
-public partial class UserViewModel : BaseViewModel
+public class UserViewModel(UserServices userServices) : BaseViewModel
 {
     public string UserEmail => _userServices.LoggedInUser.Email;
     public string UserName => _userServices.LoggedInUser.Username;
     public string HeaderMainText => $"Welcome, {UserName}!";
     
-    private readonly UserServices _userServices;
-
-    public UserViewModel(UserServices userServices)
-    {
-        _userServices = userServices;
-    }
+    private readonly UserServices _userServices = userServices;
 }
