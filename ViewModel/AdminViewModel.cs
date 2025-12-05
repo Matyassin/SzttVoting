33212470c5167b0ffdbd29cdf4acfaef7651fef8
realdaveeeed <fabricziusdavid@gmail.com>
2025-12-05 15:04:27 +0100

    Fixed AdminView

--- ViewModel/AdminViewModel.cs
+++ ViewModel/AdminViewModel.cs
@@ -5,10 +5,12 @@ namespace ViewModel;
 public class AdminViewModel : BaseViewModel
 {
     public UserServices UserServices { get; private set; }
+    public PollServices PollServices { get; private set; }
 
     public AdminViewModel(UserServices userServices)
     {
         UserServices = userServices;
+        PollServices = new PollServices();
 
     }
 }
