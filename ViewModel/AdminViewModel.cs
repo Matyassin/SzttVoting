c88556a4c532a524daf1d5439bb4865e5e613aee
Mátyás Végh <veghmatya@gmail.com>
2025-12-06 16:23:40 +0100

    Admin View header welcome messages now print the actual username of the given admin, not just "admin" :)

--- ViewModel/AdminViewModel.cs
+++ ViewModel/AdminViewModel.cs
@@ -7,10 +7,15 @@ public class AdminViewModel : BaseViewModel
     public UserServices UserServices { get; private set; }
     public PollServices PollServices { get; private set; }
 
+    public string Username { get; private set; }
+
     public AdminViewModel(UserServices userServices)
     {
         UserServices = userServices;
         PollServices = new PollServices();
+
+        Username = userServices.LoggedInUser.Username;
+
         PollServices.Load();
     }
 }
